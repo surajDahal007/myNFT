c2e442cd2ad5e3965c3e9896bb713beb00e1dc29
Suraj Dahal <96937477+surajDahal007@users.noreply.github.com>
2022-09-18 17:25:58 +0545

    Add files via upload

---
+++ NFT.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract NFT is ERC721URIStorage{
+    address public owner;
+    uint8 private tokenID;
+    string public tokenURL="https://gateway.pinata.cloud/ipfs/QmVoS9sHwPajxSAdFPFa3jAqGET3fAjt5yispvWoL4sQ4B";
+
+    constructor() ERC721("Suraj","SS"){
+        owner=msg.sender;
+    }
+
+
+    function mintNFT() public{
+        require(tokenID==0,"ONLY ONE TOKEN IS ALLOWED");
+        require(owner==msg.sender,"ONLY OWNER CAN MINT");
+        _safeMint(msg.sender,tokenID);
+        _setTokenURI(tokenID,tokenURL);
+        tokenID++;
+    }
+}
